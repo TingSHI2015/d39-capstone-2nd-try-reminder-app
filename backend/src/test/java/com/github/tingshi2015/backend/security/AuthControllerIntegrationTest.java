be77293c0b2e181d8805f27958838cdd88fa8a85
tingSHI <ting.shi2015@gmail.com>
2024-06-16 19:48:39 +0200

    Add Integration Tests for AuthController (not successful!!!)

---
+++ backend/src/test/java/com/github/tingshi2015/backend/security/AuthControllerIntegrationTest.java
@@ -0,0 +1,59 @@
+package com.github.tingshi2015.backend.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "test-user")
+    void testGetMe_withLoggedInUser_expectUsername() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/auth/me"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("?"));
+    }
+
+    @Test
+    @DirtiesContext
+    void testGetMe_withLoggedInUser_expectUsername2() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/auth/me")
+                        .with(oidcLogin().userInfoToken(token -> token
+                                .claim("login", "github-username")
+                                .claim("avatar_url", "testAvatarUrl")
+                        ))
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                //.andExpect(MockMvcResultMatchers.content().string("github-username"))
+                .andExpect(MockMvcResultMatchers.content().json(
+            """
+                        {
+                        "username": "github-username",
+                        "avatarUrl": "testAvatarUrl"
+                        }
+                        """
+                            ));
+    }
+
+    @Test
+    void testGetMe_withoutLoggedInUser_expectStatus401() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/auth/me"))
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError())
+                .andExpect(MockMvcResultMatchers.content().string("?"));
+    }
+}
