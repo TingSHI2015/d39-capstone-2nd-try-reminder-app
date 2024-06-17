c96157c05a25f5f5a6564b779fc83ce98ea442e4
tingSHI <ting.shi2015@gmail.com>
2024-06-17 12:31:59 +0200

    - Add / for the axios.get("api/auth/me") in HomePage - successfully revise Integration test for AuthController

--- backend/src/test/java/com/github/tingshi2015/backend/security/AuthControllerIntegrationTest.java
+++ backend/src/test/java/com/github/tingshi2015/backend/security/AuthControllerIntegrationTest.java
@@ -4,13 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 
 @SpringBootTest
