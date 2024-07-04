e94d8b4c1805d09b62d590aef55acab939177d77
tingSHI <ting.shi2015@gmail.com>
2024-07-04 10:11:04 +0200

    Frontend (with bug version): - add "showUpcomingReminder" State in UpcomingReminderGallery.tsx

--- frontend/src/components/UpcomingReminderGallery.tsx
+++ frontend/src/components/UpcomingReminderGallery.tsx
@@ -3,39 +3,57 @@ import {Reminder} from "../types/Reminder.ts";
 import axios from "axios";
 
 export default function UpcomingReminderGallery() {
-    const [upcomingReminders, setUpcomingReminders] = useState<Reminder[]>([])
+    const [upcomingReminders, setUpcomingReminders] = useState<Reminder[]>([]);
+    const [showUpcomingReminder, setShowUpcomingReminder] = useState<boolean>(false);
 
     useEffect(() => {
-        getUpcomingReminders()
 
-        const interval = setInterval(() => {getUpcomingReminders();
+        getUpcomingReminders();
+
+        const interval = setInterval(() => {
+            getUpcomingReminders();
         }, 60000);
 
-        return () => clearInterval(interval);
+        return () => clearInterval(interval); //Cleanup the interval on unmount
 
     }, []);
 
 
     const getUpcomingReminders = () => {
-        axios.get("api/reminders/upcoming")
-            .then(response => setUpcomingReminders(response.data))
-            .catch(error => console.error("Error getting upcoming reminders", error))
-            .finally(() => {
-                console.log("getUpcomingReminder_successful")
+        axios.get("/api/reminders/upcoming")
+            .then(response => {
+                if(response.data.length > 0) {
+                    setUpcomingReminders(response.data);
+                    setShowUpcomingReminder(true);
+                }
             })
+            .catch(error => console.error("Error getting upcoming reminders", error))
+            .finally(() => {console.log("getUpcomingReminder_successful")})
+    }
+
+    const handleClose = () => {
+        setShowUpcomingReminder(false);
+        setUpcomingReminders([]);
     }
 
     return(
         <div>
-            <h3>Upcoming Reminders</h3>
-            <ul>
-                {upcomingReminders.map(reminder => (
-                <li key={reminder.id}>
-                    {reminder.name} at {reminder.date} {reminder.time}
-                </li>
-                ))}
-            </ul>
+            {showUpcomingReminder && (
+                <div>
+                    <h3>Upcoming Reminders</h3>
+                    <ul>
+                        {upcomingReminders.map(reminder => (
+                            <li key={reminder.id}>
+                                {reminder.name} at {reminder.date} {reminder.time}
+                            </li>
+                        ))}
+                    </ul>
+                    <button onClick={handleClose}>Close</button>
+                </div>
+            )
+            }
         </div>
+
     )
 
 }
